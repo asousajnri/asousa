f39ac45433553289c67b0cadd4d06a2d73f910a0
Antônio Sousa <a.sousa.jnri@gmail.com>
2020-03-01 19:57:53 -0300

    Criando Components Icon

---
+++ frontend/src/components/LayoutIcons/index.jsx
@@ -0,0 +1,23 @@
+import Facebook from "./ComponentsIcon/Facebook";
+import Twitter from "./ComponentsIcon/Twitter";
+import Instagram from "./ComponentsIcon/Instagram";
+import Linkedin from "./ComponentsIcon/Linkedin";
+
+import Mortarboard from "./ComponentsIcon/Mortarboard";
+import Skills from "./ComponentsIcon/Skills";
+import Project from "./ComponentsIcon/Project";
+import ContactUs from "./ComponentsIcon/ContactUs";
+import Work from "./ComponentsIcon/Work";
+
+export { 
+  Facebook, 
+  Twitter, 
+  Instagram, 
+  Linkedin,
+
+  Mortarboard,
+  Skills,
+  Work,
+  Project,
+  ContactUs
+};

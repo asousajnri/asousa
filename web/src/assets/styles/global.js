b983c1544e6eaca1da6268117c20d8dc68c1ba82
Antônio Sousa <a.sousa.jnri@gmail.com>
2020-08-11 21:57:45 -0300

    Adicionar base do React JS.

---
+++ web/src/assets/styles/global.js
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyled = createGlobalStyle`
+  :root {
+    font-size: 60%;
+
+    --background-color: #E9EBEE;
+    --primary-color: #EA178C;
+    --secundary-color: #1CB517;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: bordex-box;
+  }
+
+  html, body, #root {
+    height: 100vh;
+  }
+
+  body {
+    background-color: var(--background-color);
+  }
+
+  #root {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  body,
+  input,
+  button,
+  textarea {
+    font: 500 1.6rem Open Sans;
+  }
+`;
+
+export default GlobalStyled;
---
+++ web/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import GlobalStyled from "./assets/styles/global";
+import App from "./App";
+
+ReactDOM.render(
+  <>
+    <GlobalStyled />
+    <App />
+  </>,
+  document.getElementById("root")
+);

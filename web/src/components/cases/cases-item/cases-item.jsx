e0d13d97561ab6e428704aa02f046bf7d61d4f97
Antônio Sousa <antonio.sousa@indexdigital.com.br>
2020-08-20 09:21:13 -0300

    Eslint formated.

--- web/src/components/cases/cases-item/cases-item.jsx
+++ web/src/components/cases/cases-item/cases-item.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import MyIcons from '../../icons';
 
@@ -8,21 +8,17 @@ import {
   ClickMe,
 } from './cases-item-styles';
 
-const CasesItem = ({ imageUrl, name, background }) => {
-  const [hover, setHover] = useState(false);
-
-  return (
-    <StyledCasesItem>
-      <img src={imageUrl} alt={name} />
-      <StyledOverlay background={background}>
-        <h2>{name}</h2>
-        <ClickMe>
-          <MyIcons.Select width="2.5rem" fill="#FFFFFF" />
-          <span>Clique-me!</span>
-        </ClickMe>
-      </StyledOverlay>
-    </StyledCasesItem>
-  );
-};
+const CasesItem = ({ imageUrl, name, background }) => (
+  <StyledCasesItem>
+    <img src={imageUrl} alt={name} />
+    <StyledOverlay background={background}>
+      <h2>{name}</h2>
+      <ClickMe>
+        <MyIcons.Select width="2.5rem" fill="#FFFFFF" />
+        <span>Clique-me!</span>
+      </ClickMe>
+    </StyledOverlay>
+  </StyledCasesItem>
+);
 
 export default CasesItem;
--- web/src/components/cases/cases.jsx
+++ web/src/components/cases/cases.jsx
@@ -4,36 +4,34 @@ import CasesItem from './cases-item';
 
 import { StyledCases } from './cases-styles';
 
-const Cases = () => {
-  return (
-    <StyledCases>
-      <CasesItem
-        background="primary"
-        imageUrl="https://picsum.photos/id/200/600/600"
-        name="Teste"
-      />
-      <CasesItem
-        background="primary"
-        imageUrl="https://picsum.photos/id/201/600/600"
-        name="Teste"
-      />
-      <CasesItem
-        background="primary"
-        imageUrl="https://picsum.photos/id/202/600/600"
-        name="Teste"
-      />
-      <CasesItem
-        background="primary"
-        imageUrl="https://picsum.photos/id/203/600/600"
-        name="Teste"
-      />
-      <CasesItem
-        background="primary"
-        imageUrl="https://picsum.photos/id/204/600/600"
-        name="Teste"
-      />
-    </StyledCases>
-  );
-};
+const Cases = () => (
+  <StyledCases>
+    <CasesItem
+      background="primary"
+      imageUrl="https://picsum.photos/id/200/600/600"
+      name="Teste"
+    />
+    <CasesItem
+      background="primary"
+      imageUrl="https://picsum.photos/id/201/600/600"
+      name="Teste"
+    />
+    <CasesItem
+      background="primary"
+      imageUrl="https://picsum.photos/id/202/600/600"
+      name="Teste"
+    />
+    <CasesItem
+      background="primary"
+      imageUrl="https://picsum.photos/id/203/600/600"
+      name="Teste"
+    />
+    <CasesItem
+      background="primary"
+      imageUrl="https://picsum.photos/id/204/600/600"
+      name="Teste"
+    />
+  </StyledCases>
+);
 
 export default Cases;

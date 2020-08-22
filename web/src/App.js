74ab52f7b6ec8ed2e03130c27e7c3c7f94da3220
Antônio Sousa <a.sousa.jnri@gmail.com>
2020-08-22 17:41:52 -0300

    Revert "Add eslint para TypeScript."
    This reverts commit 2ba6a906913866f69b998b6ed0588f96701166c3.
    
    # Conflicts:
    #	web/.eslintrc.json
    #	web/src/index.ts

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
-const App: React.FC = () => {
-  return <h1></h1>;
-};
+import {
+  Profile,
+  Section,
+  Cases,
+  Skills,
+  Career,
+  Contact,
+} from './components';
+
+const App = () => (
+  <>
+    <Profile />
+    <Section color="primary" title="Cases">
+      <Cases />
+    </Section>
+    <Section color="secondary" title="Habilidades">
+      <Skills />
+    </Section>
+    <Section color="primary" title="Carreira">
+      <Career />
+    </Section>
+    <Section
+      as="footer"
+      color="secondary"
+      title="Contate-me"
+      lastSection
+    >
+      <Contact />
+    </Section>
+  </>
+);
 
 export default App;

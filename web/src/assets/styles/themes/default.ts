b443863601e77de5d776a21cd9651943a1251419
Antônio Sousa <a.sousa.jnri@gmail.com>
2020-08-22 23:12:51 -0300

    Inicar typescript

---
+++ web/src/assets/styles/themes/default.ts
@@ -0,0 +1,23 @@
+export default {
+  title: "default",
+
+  colors: {
+    primary: "#EA178C",
+    secondary: "#1CB517",
+
+    white: "#FFFFFF",
+    black: "#000000",
+    grey: "#8a8a8a",
+    text: "#000000",
+  },
+
+  page: {
+    body: "#E9EBEE",
+  },
+
+  profile: {
+    info: "#FFFFFF",
+    icon: "#D3D3D3",
+    icon_hover: "#000000",
+  },
+};

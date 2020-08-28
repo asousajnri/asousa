8ee4949d702c14e6f83e004a2ac67c68e4665997
Antônio Sousa <a.sousa.jnri@gmail.com>
2020-08-28 12:11:17 -0300

    Add mais models

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,13 +3,24 @@ import multer from 'multer';
 
 import uploadConfig from './configs/upload';
 
-import ProfileController from './controllers/ProfileController';
+import Controllers from './controllers';
 
 const upload = multer(uploadConfig);
-
 const routes = Router();
 
-routes.get('/profile', ProfileController.index);
-routes.post('/profile', upload.single('photo'), ProfileController.store);
+routes.get('/profile', Controllers.Profile.index);
+routes.post('/profile', upload.single('photo'), Controllers.Profile.store);
+
+routes.get('/cases', Controllers.Cases.index);
+routes.post('/cases', upload.single('coverImage'), Controllers.Cases.store);
+
+routes.get('/social-network', Controllers.SocialNetwork.index);
+routes.post('/social-network', Controllers.SocialNetwork.store);
+
+routes.get('/skills', Controllers.Skills.index);
+routes.post('/skills', upload.single('logo'), Controllers.Skills.store);
+
+routes.get('/career', Controllers.Career.index);
+routes.post('/career', upload.single('logo'), Controllers.Career.store);
 
 export default routes;

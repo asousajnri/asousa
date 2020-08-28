ca14b3635b156257c23d0f9440d0e0c12573647a
Antônio Sousa <a.sousa.jnri@gmail.com>
2020-08-28 16:31:48 -0300

    Reset loco.

--- backend/src/interfaces/Skills.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-
-export default interface SkillsInterface extends Document {
-  title: string;
-  logo: string;
-  level: string;
-}
--- backend/src/interfaces/SocialNetwork.ts
+++
@@ -1,6 +0,0 @@
-import { Document } from 'mongoose';
-
-export default interface SocialNetworkInterface extends Document {
-  title: string;
-  link: string;
-}

697bc4271d9a9c096ef59ba5275a3d41b3dfc3e3
Antônio Sousa <a.sousa.jnri@gmail.com>
2020-08-28 09:19:00 -0300

    Rename folder baced to backend.

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,24 +3,13 @@ import multer from 'multer';
 
 import uploadConfig from './configs/upload';
 
-import Controllers from './controllers';
+import ProfileController from './controllers/ProfileController';
 
 const upload = multer(uploadConfig);
+
 const routes = Router();
 
-routes.get('/profile', Controllers.Profile.index);
-routes.post('/profile', upload.single('photo'), Controllers.Profile.store);
-
-routes.get('/cases', Controllers.Cases.index);
-routes.post('/cases', upload.single('coverImage'), Controllers.Cases.store);
-
-routes.get('/social-network', Controllers.SocialNetwork.index);
-routes.post('/social-network', Controllers.SocialNetwork.store);
-
-routes.get('/skills', Controllers.Skills.index);
-routes.post('/skills', upload.single('logo'), Controllers.Skills.store);
-
-routes.get('/career', Controllers.Career.index);
-routes.post('/career', upload.single('logo'), Controllers.Career.store);
+routes.get('/profile', ProfileController.index);
+routes.post('/profile', upload.single('photo'), ProfileController.store);
 
 export default routes;

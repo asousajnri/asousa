6d8e91fb9fce96ff9e964f0498d6c870bfc5cfcc
Antônio Sousa <antonio.sousa@indexdigital.com.br>
2020-08-31 10:40:10 -0300

    Deixar só files backend.

--- web/src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Title } from './title';
-export { default as MyIcons } from './my-icons';
-export { default as Form } from './form';
-export { default as Button } from './button';

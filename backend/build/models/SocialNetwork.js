b780c69ff4cb57c8969f5aff74bf3413f629b552
Antônio Sousa <a.sousa.jnri@gmail.com>
2020-08-31 22:14:14 -0300

    Remover pasta backend

--- backend/build/models/SocialNetwork.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose_1 = require("mongoose");
-var SocialNetworkSchema = new mongoose_1.Schema({
-    title: String,
-    link: String,
-}, { timestamps: true });
-var SocialNetwork = mongoose_1.model('SocialNetwork', SocialNetworkSchema);
-exports.default = SocialNetwork;

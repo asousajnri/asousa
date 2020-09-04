8561ca74e50ed2e86f5a919d15c72a151e9d1b28
Antônio Sousa <a.sousa.jnri@gmail.com>
2020-09-04 13:11:30 -0300

    Build com multerS3
    só vai fela.

--- src/interfaces/Cases.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-
-export default interface CasesInterface extends Document {
-  title: string;
-  link: string;
-  coverImage: string;
-  description: string;
-  category: string;
-}
